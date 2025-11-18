f16b5fcaaa4e3aff540440e6990e3ab3e3964a36
dishbee <info@dishbee.com>
2025-11-19 00:11:48 +0100

    Remove Docker and build script files

--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Install system dependencies
-apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng
-
-# Install Python dependencies
-pip install -r requirements.txt
